a81a2594a9a6e7c05705ef04f8d900c1fdba984d
mrstraining <milena.reis.sousa@hotmail.com>
2025-03-29 16:52:00 +0000

    1º VS

---
+++ D01_Git/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D01_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("1º VS");
+
+        }
+    }
+}
